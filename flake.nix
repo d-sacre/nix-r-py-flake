103e530247db9591a18bebedfa1c2bfdc74c3efa
d-sacre <daniel.sacre@gmx.de>
2024-04-20 17:16:58 +0200

    tried to achieve R Version Override with Overlay

--- flake.nix
+++ flake.nix
@@ -6,15 +6,26 @@
   outputs = { self, nixpkgs }: {
     devShell.x86_64-linux =
       let
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # sets/limits??? (indirectly) the Development System to x86_64-linux
-        custom-r-version = pkgs.rPackages.overrideAttrs (_: {
-          version = "4.3.1";
-        });
+        system = "x86_64-linux";
+        pkgs = nixpkgs.legacyPackages.${system}; # sets/limits??? (indirectly) the Development System to x86_64-linux
+        custom-r-version = import nixpkgs { 
+          inherit system; 
+          overlays = [ 
+            final: prev:
+            {
+              rPackages = prev.rPackages.override {
+                overrides = {
+                  version = "4.3.1";
+                };
+              };
+            }
+          ]; 
+        };
         rpkgs = builtins.attrValues {
           inherit (custom-r-version) forecast lubridate tidyr stringr forcats testthat ggplot2 plotly gridExtra naniar yaml glue caret prophet readxl feather tidyverse shiny;
         };
         systemPackages = builtins.attrValues {
-          inherit (pkgs) R glibcLocales nix ;
+          inherit (custom-r-version) R glibcLocales nix ;
         };
       in pkgs.mkShell {
         nativeBuildInputs = [ # for the system the Developement Environment is running on!
