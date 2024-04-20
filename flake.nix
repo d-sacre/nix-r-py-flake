b1c3d1614cad61ba584cc96771df12f5941b5508
d-sacre <daniel.sacre@gmx.de>
2024-04-20 16:54:55 +0200

    tried to fix overideAttrs bug

--- flake.nix
+++ flake.nix
@@ -7,9 +7,9 @@
     devShell.x86_64-linux =
       let
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # sets/limits??? (indirectly) the Development System to x86_64-linux
-        custom-r-version = pkgs.rPackages.overrideAttrs (oldAttrs: {
+        custom-r-version = pkgs.rPackages.overrideAttrs{
           version = "4.3.1";
-        });
+        };
         rpkgs = builtins.attrValues {
           inherit (custom-r-version) forecast lubridate tidyr stringr forcats testthat ggplot2 plotly gridExtra naniar yaml glue caret prophet readxl feather tidyverse shiny;
         };
