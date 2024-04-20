95d5ce6d0f623f78ff9fb0d1bc699fb81884f60e
d-sacre <daniel.sacre@gmx.de>
2024-04-20 17:54:46 +0200

    experimented with custom overide

--- flake.nix
+++ flake.nix
@@ -4,54 +4,27 @@
   description = "R/Python Development Flake";
 
   outputs = { self, nixpkgs }: {
+
     devShell.x86_64-linux =
       let
         system = "x86_64-linux";
-        pkgs = nixpkgs.legacyPackages.${system}; # sets/limits??? (indirectly) the Development System to x86_64-linux
-        # custom-r-version = import nixpkgs { 
-        #   inherit system; 
-        #   overlays = [ 
-        #     (final: prev:
-        #     {
-        #       rPackages = prev.rPackages.override {
-        #         overrides = {
-        #           version = "4.1.3";
-        #         };
-        #       };
-        #     })
-        #   ]; 
-        # };
-        # rpkgs = builtins.attrValues {
-        #   inherit (custom-r-version) 
-        #   # forecast 
-        #   lubridate 
-        #   tidyr 
-        #   stringr 
-        #   # forcats 
-        #   testthat 
-        #   # ggplot2 
-        #   plotly 
-        #   gridExtra 
-        #   naniar 
-        #   yaml 
-        #   glue 
-        #   # caret 
-        #   prophet 
-        #   readxl 
-        #   feather 
-        #   tidyverse 
-        #   shiny;
-        # };
-        # systemPackages = builtins.attrValues {
-        #   inherit (custom-r-version) R glibcLocales nix ;
-        # };
+        # pkgs = nixpkgs.legacyPackages.${system}; # sets/limits??? (indirectly) the Development System to x86_64-linux
+        pkgs = mkPkgs (import nixpkgs) [
+          (_: super: {
+            R = super.R.overrideAttrs { version = "4.1.3"; };
+          })
+        ];
+        rpkgs = builtins.attrValues {
+          inherit (pkgs.rPackages) forecast lubridate tidyr stringr forcats testthat ggplot2 plotly gridExtra naniar yaml glue caret prophet readxl feather tidyverse shiny;
+        };
+        systemPackages = builtins.attrValues {
+          inherit (pkgs) R glibcLocales nix ;
+        };
+
       in pkgs.mkShell {
         nativeBuildInputs = [ # for the system the Developement Environment is running on!
-          # rpkgs
-          # systemPackages
-          pkgs.rstudioWrapper.override {
-            packages = with pkgs.rPackages; [ dplyr ggplot2 reshape2 ];
-          }
+          rpkgs
+          systemPackages
         ];
       };
   };
